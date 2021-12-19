8aa57b720898dcfb15be97b091cac56a9a9f2325
omari <omari.jalagania@gmail.com>
2021-12-19 13:56:40 +0400

    dummy home content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 import SignIn from "./components/SignIn";
 
 function App() {
   return (
     <>
-      <Home />
+      <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </>
