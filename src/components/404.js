68cff90c11e886d699d7a08fc85f8139c6c3f79b
omari <omari.jalagania@gmail.com>
2021-12-19 17:17:58 +0400

    not found links

--- src/components/404.js
+++ src/components/404.js
@@ -1,4 +1,7 @@
-/* This example requires Tailwind CSS v2.0+ */
+import React from "react";
+
+import { Link } from "react-router-dom";
+
 export default function PageNotFound() {
   return (
     <div className="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
@@ -17,18 +20,18 @@ export default function PageNotFound() {
               </p>
             </div>
             <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Go back home
-              </a>
-              <a
-                href="#"
+                მთავარ გვერდზე გადასვლა
+              </Link>
+              <Link
+                to="/contact"
                 className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Contact support
-              </a>
+                კონტაქტი
+              </Link>
             </div>
           </div>
         </main>
