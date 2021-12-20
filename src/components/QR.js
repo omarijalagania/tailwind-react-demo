ea5045b029a9e58116734bcfe3192318b4a14551
Irakli <koregm@gmail.com>
2021-12-20 17:29:33 +0400

    Add style on QR component

--- src/components/QR.js
+++ src/components/QR.js
@@ -2,17 +2,19 @@ import React, { useState } from "react";
 import QRCode from "react-qr-code";
 
 function QR() {
-  const [qr, setQr] = useState("https://www.facebook.com");
+  const [qr, setQr] = useState("https://www.google.ge");
 
   return (
-    <div className="p-20">
-      <QRCode value={qr} />
-      <input
-        className="m-3 border-2"
-        onChange={(e) => setQr(e.target.value)}
-        type="text"
-        value={qr}
-      />
+    <div className="flex justify-center">
+      <div className="p-20">
+        <QRCode value={qr} />
+        <input
+          className="mt-3 p-1 w-full border-2"
+          onChange={(e) => setQr(e.target.value)}
+          type="text"
+          value={qr}       
+        />
+      </div>
     </div>
   );
 }
