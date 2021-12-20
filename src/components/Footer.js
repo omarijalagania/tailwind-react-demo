4044dc19189acf49b5ec5ac4076975f0efece525
Irakli <koregm@gmail.com>
2021-12-20 18:10:31 +0400

    Translate footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -116,7 +116,7 @@ export default function Footer() {
         Footer
       </h2>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
+        {/* <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
           <div className="grid grid-cols-2 gap-8 xl:col-span-4">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               <div>
@@ -251,20 +251,20 @@ export default function Footer() {
               </fieldset>
             </form>
           </div>
-        </div>
+        </div> */}
         <div className="border-t border-gray-700 pt-8 lg:flex lg:items-center lg:justify-between xl:mt-0">
           <div>
             <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-              Subscribe to our newsletter
+              გამოიწერეთ ჩვენი საინფორმაციო ბიულეტენი
             </h3>
             <p className="mt-2 text-base text-gray-300">
-              The latest news, articles, and resources, sent to your inbox
-              weekly.
+              უახლესი ამბები, სტატიები და რესურსები, ყოველკვირეულად იგზავნება
+              თქვენს ინბოქსში.
             </p>
           </div>
           <form className="mt-4 sm:flex sm:max-w-md lg:mt-0">
             <label htmlFor="email-address" className="sr-only">
-              Email address
+              ელ. ფოსტა
             </label>
             <input
               type="email"
@@ -273,14 +273,14 @@ export default function Footer() {
               autoComplete="email"
               required
               className="appearance-none min-w-0 w-full bg-white border border-transparent rounded-md py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white focus:border-white focus:placeholder-gray-400 sm:max-w-xs"
-              placeholder="Enter your email"
+              placeholder="ელ. ფოსტა"
             />
             <div className="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
               <button
                 type="submit"
                 className="w-full bg-indigo-500 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
               >
-                Subscribe
+                გამოწერა
               </button>
             </div>
           </form>
@@ -299,7 +299,7 @@ export default function Footer() {
             ))}
           </div>
           <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
-            &copy; 2020 Workflow, Inc. All rights reserved.
+            &copy; 2021 ყველა უფლება დაცულია
           </p>
         </div>
       </div>
