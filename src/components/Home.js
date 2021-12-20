20c362da47e381f15b61a943a84f2da629342bed
Irakli <koregm@gmail.com>
2021-12-20 14:34:18 +0400

    Move SingIn to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,13 @@
 import React from "react";
 
-import Hero from "./Hero";
-import Content from "./Content";
+import SignIn from "./SignIn";
+import Registration from "./Registration";
 import Teams from "./Teams";
-import Featured from "./Featured";
+import QR from "./QR";
 function Home() {
   return (
     <div>
-      <Hero />
-      <Content />
-      <Featured />
-      <Teams />
+      <SignIn />
     </div>
   );
 }
