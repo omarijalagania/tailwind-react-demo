9abe1fbcb47d04ecbb4205244cbc7e8a6c7c041a
omari <omari.jalagania@gmail.com>
2021-12-23 00:45:27 +0400

    button color

--- src/components/Signature.js
+++ src/components/Signature.js
@@ -29,6 +29,7 @@ function Signature() {
           }}
         />
       </div>
+
       {imageURL ? (
         <img
           src={imageURL}
@@ -41,6 +42,7 @@ function Signature() {
           }}
         />
       ) : null}
+
       {/* Button to trigger save canvas image */}
 
       <div className="flex flex-col sm:flex-row justify-center p-1">
@@ -60,9 +62,12 @@ function Signature() {
         </button>
 
         <button
+          disabled={imageURL ? false : true}
           onClick={reset}
           type="submit"
-          className="w-25 m-1 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className={`w-25 m-1 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${
+            imageURL ? "bg-blue-600 hover:bg-blue-700" : "bg-gray-600 "
+          }  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         >
           წაშლა
         </button>
